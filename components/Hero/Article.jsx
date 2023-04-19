ace28d45302bc395db91035438a0cf56612a8b09
Serki <nemli.serkan2@gmail.com>
2023-04-19 16:06:59 +0300

    Styling

--- components/Hero/Article.jsx
+++ components/Hero/Article.jsx
@@ -8,15 +8,15 @@ const Article = () => {
     <motion.article
       initial={motions.rightToLeft.initial}
       animate={motions.rightToLeft.animate}
-      className="w-2/3 border-l-slate-200 h-full border-l-2 p-2  flex flex-col items-start "
+      className="w-2/3 h-full border-l-gray-500 border-opacity-30  border-l-2 p-2  space-y-2  flex flex-col items-start "
     >
       <h1 className="text-4xl font-bold">Hero Title</h1>
       <p className="">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
         voluptate, quod, quia, voluptates quae voluptatibus quibusdam
         accusantium quas quidem nemo voluptatum. Quisquam, quae. Quisquam
-        voluptate, quod, quia, voluptates quae voluptatibus quibusdam
-        accusantium quas quidem nemo voluptatum. Quisquam, quae.
+        voluptate, quod, quia, voluptates quae voluptatibuadadaada asdad asda
+        asdas
       </p>
     </motion.article>
   );
