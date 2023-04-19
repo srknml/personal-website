0b29f26cae479819fcf77bfc3ad68ce5a11a29b7
Serki <nemli.serkan2@gmail.com>
2023-04-19 14:50:16 +0300

    Hero component created.

---
+++ components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section>
+      <div>Image Box</div>
+
+      <div>Text Box</div>
+    </section>
+  );
+};
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
 
       <main>
         <Header />
+        <Hero />
       </main>
     </div>
   );
