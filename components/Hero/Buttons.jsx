d6dd7c643fbcf96c3fc0d0f1b734b480d1568570
Serki <nemli.serkan2@gmail.com>
2023-05-10 17:07:09 +0300

    Buttons fixed for its purposes

--- components/Hero/Buttons.jsx
+++ components/Hero/Buttons.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Utils from "../../Utils/index";
+import Link from "next/link";
+import Router from "next/router";
 
 const Buttons = () => {
   {
@@ -23,11 +25,21 @@ const Buttons = () => {
       }}
       className="flex flex-row justify-center p-2 w-full mx-auto  "
     >
-      <button className="md-max:w-1/2  w-1/4 border-2 border-transparent bg-[#e69417] hover:bg-transparent hover:border-[#e69417] hover:border-2 hover:text-white text-gray-950    font-bold m-1 py-2 px-5 rounded-xl">
-        Button
-      </button>
-      <button className=" md-max:w-1/2 w-1/4 border-2 border-gray-800  hover:border-[#e69417] hover:border-2 hover:text-white text-[#e69417] font-bold m-1 py-2 px-5 rounded-lg">
-        Button
+      <Link
+        href={
+          "https://docs.google.com/document/d/e/2PACX-1vTcTgw8hrDRzvzu_E8qisaA9tea33B6104PnywlrK1SG9tjx1-g8Pwc5yePtsd56BErV8-svFIegFWm/pub"
+        }
+      >
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          className="md-max:w-1/2 text-center w-1/4 border-2 border-transparent bg-[#e69417] hover:bg-transparent hover:border-[#e69417] hover:border-2 hover:text-white text-gray-950    font-bold m-1 py-2 px-5 rounded-xl"
+        >
+          Resume
+        </a>
+      </Link>
+      <button onClick={() => Router.push('mailto:contact@serkannemli.com')} className=" md-max:w-1/2 w-1/4 border-2 border-gray-800  hover:border-[#e69417] hover:border-2 hover:text-white text-[#e69417] font-bold m-1 py-2 px-5 rounded-lg">
+        Contact
       </button>
     </motion.div>
   );
