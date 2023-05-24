f6fb117513d1dc16c626e7419a405c61871fc5a4
Serki <nemli.serkan2@gmail.com>
2023-05-24 18:38:53 +0300

    Cleaning

--- components/ThemeSwitch.jsx
+++ components/ThemeSwitch.jsx
@@ -2,14 +2,8 @@ import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import image from "../public/image.jpg";
 import Image from "next/image";
-import { FaAws, FaReact, FaHtml5 } from "react-icons/fa";
 const Switch = ({ isOn, ...rest }) => {
-  // initialize the customClassName according to the
-  // state of the "isOn" using ternary operator
   const customClassName = `toggleSwitch ${isOn ? "on" : "off"}`;
-
-  // initialize the src according to the
-  // state of the "isOn" using ternary operator
 
   return (
     <motion.div animate className={`${customClassName} `} {...rest}>
@@ -24,8 +18,6 @@ const ThemeSwitch = () => {
   const [isOn, setIsOn] = useState(false);
 
   useEffect(() => {
-    // background-color changes every time "isOn"
-    // changes using JavaScript DOM methods
     document.body.style.backgroundColor = isOn ? "#1c1c1c" : "#ffffff";
   }, [isOn]);
 
