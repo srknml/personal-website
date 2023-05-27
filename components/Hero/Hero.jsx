ebc2b204bc74bf6d43109b9ceaf1ec64f9e8eddb
Schude <nemli.serkan2@gmail.com>
2023-05-27 22:12:41 +0300

    Hero

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Imagebox from "./Imagebox";
 import Article from "./Article";
 import Buttons from "./Buttons";
+import {useTheme} from '../../context/Theme'
 
 const Hero = () => {
+
+  const {isDark} = useTheme()
+
   return (
     <section
       id="home"
-      className=" mock-border md-max:space-y-5 w-full custom-h flex  flex-col justify-center items-center flex-grow align-center md:w-2/3 mx-auto p-2 space-y-10   "
+      className={` ${isDark ? "dark" : "light"}  md-max:space-y-5 w-full custom-h flex  flex-col justify-center items-center flex-grow align-center md:w-2/3 mx-auto p-2 space-y-10`}
     >
       <div className=" md-max:space-y-5 flex flex-col items-center md:flex md:flex-row  my-autp p-2 justify-center align-middle space-x-4">
         <Imagebox />
