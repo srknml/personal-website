f85c3e0ac7873fc49976893eb84eaa0da90dd10d
Serki <nemli.serkan2@gmail.com>
2023-06-01 00:16:08 +0300

    Dark theme

--- components/Experiences/AccardionItem.jsx
+++ components/Experiences/AccardionItem.jsx
@@ -7,37 +7,32 @@ import {
 } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 import ExperiencePoint from "./ExperiencePoint";
-import { useTheme } from '../../context/Theme'
-
+import { useTheme } from "../../context/Theme";
 
 const AccordionBox = ({ title, date, points }) => {
-  const { isDark } = useTheme()
+  const { isDark } = useTheme();
 
-  
   return (
     <AccordionItem className="flex flex-col flex-grow p-2 text-lg justify-center align-top w-full">
       {({ isExpanded }) => (
-        (
-          <>
-            <h2
-              className={
-                (isExpanded ? " shadow-sm shadow-[#1623338e]  " : " ") +
-                "py-4 px-2 w-full bg-[#06142473] hover:bg-[#052144]"
-              }
-            >
-              <AccordionButton className="w-full">
-                <AccordionIcon />
+        <>
+          <h2
+            className={`${isExpanded ? "shadow-sm shadow-[#1623338e]" : ""} ${
+              isDark ? "hover:bg-[#121e2c38] " : " bg-slate-200 hover:bg-slate-300"
+            }    py-4 px-2 w-full   `}
+          >
+            <AccordionButton className="w-full">
+              <AccordionIcon />
 
-                <Box pl={8} as="span" textAlign="left">
-                  {title} - <i className="text-xs">{date} </i>
-                </Box>
-              </AccordionButton>
-            </h2>
-            <AccordionPanel className="p-2 mt-2 text-base inline-block w-full bg-[#24467044] ">
-              <ExperiencePoint points={points} />
-            </AccordionPanel>
-          </>
-        )
+              <Box pl={8} as="span" textAlign="left">
+                {title} - <i className="text-xs">{date} </i>
+              </Box>
+            </AccordionButton>
+          </h2>
+          <AccordionPanel className={`${isDark ?  "bg-[#24467044]": " bg-slate-100"}   p-2 mt-2 text-base inline-block w-full  bg-[#24467044] `}>
+            <ExperiencePoint points={points} />
+          </AccordionPanel>
+        </>
       )}
     </AccordionItem>
   );
