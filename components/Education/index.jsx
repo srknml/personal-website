f0172bdc83906bca9662a6bf2f5ffc1d48574bb5
Serki <nemli.serkan2@gmail.com>
2023-06-03 18:33:30 +0300

    Created json for education. Refactor Education component.

--- components/Education/index.jsx
+++ components/Education/index.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import AccordionBox from "../Experiences/AccardionItem";
-import AccordionLayout from "../Experiences/AccordionLayout";
+import EducationData from "../../public/Education.json";
 
 const Education = () => {
   return (
     <section id="education" className="sectionContainer">
       <h1 className="sectionTitle">Education</h1>
       <div className="w-full CONTAINER ">
-        <div className=" bg-slate-100 flex flex-col flex-grow p-2 text-lg justify-center align-top w-full">
-          <div className="flex w-full flex-row justify-between">
-            <p> • Kadir Has University </p>{" "}
-            <span className="text-xs !leading-7 align-middle italic">
-              {"2016 - 2020"}{" "}
-            </span>{" "}
-          </div>
-          <p className=" text-sm ml-3">
-            {" "}
-            International Trade Logistics - Full Scholarship{" "}
-          </p>
-        </div>
+        {EducationData &&
+          EducationData.map((data, index) => {
+            return (
+              <div
+                key={index}
+                className=" bg-gray-100 flex flex-col flex-grow p-2 my-2 text-lg justify-center align-top w-full"
+              >
+                <div className="flex w-full flex-row justify-between">
+                  <p> • {data.title} </p>
+                  <span className="text-xs !leading-7 align-middle italic">
+                    {data.date}{" "}
+                  </span>{" "}
+                </div>
+                <p className=" text-sm ml-3">{data.details}</p>
+              </div>
+            );
+          })}
       </div>
     </section>
   );
