f291fe4dec85255b0fb673bad5b92bc6e465b5a2
Serki <nemli.serkan2@gmail.com>
2023-06-03 18:08:39 +0300

    Education Item created for the Section.

--- components/Education/index.jsx
+++ components/Education/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import AccordionBox from "../Experiences/AccardionItem";
+import AccordionLayout from "../Experiences/AccordionLayout";
 
 const Education = () => {
   return (
     <section id="education" className="sectionContainer">
       <h1 className="sectionTitle">Education</h1>
-      <div>asd</div>
+      <div className="w-full CONTAINER ">
+        <div className=" bg-slate-100 flex flex-col flex-grow p-2 text-lg justify-center align-top w-full">
+          <div className="flex w-full flex-row justify-between">
+            <p> • Kadir Has University </p>{" "}
+            <span className="text-xs !leading-7 align-middle italic">
+              {"2016 - 2020"}{" "}
+            </span>{" "}
+          </div>
+          <p className=" text-sm ml-3">
+            {" "}
+            International Trade Logistics - Full Scholarship{" "}
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
